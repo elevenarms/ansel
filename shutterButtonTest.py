90233c8c2bcdf71e2960163308ca53c04c2eea1c
Dave Rauchwerk <elevenarms@gmail.com>
2013-11-22 00:26:50 -0600

    change pin to 22

--- shutterButtonTest.py
+++ shutterButtonTest.py
@@ -5,6 +5,6 @@ def testButton(channel):
 	print "fuck yea button press!"
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(25,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(25, GPIO.FALLING, callback=testCallback,bouncetime=300)
+GPIO.setup(22,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(22, GPIO.FALLING, callback=testCallback,bouncetime=300)
 
