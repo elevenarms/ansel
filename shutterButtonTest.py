b332290f448424726b4ae38936b513bce18dedfd
Dave Rauchwerk <elevenarms@gmail.com>
2013-11-22 00:30:40 -0600

    while

--- shutterButtonTest.py
+++ shutterButtonTest.py
@@ -1,10 +1,12 @@
 import RPi.GPIO as GPIO
 
 
-def testCallback(channel):
+def testButton(channel):
 	print "fuck yea button press!"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(22,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(22, GPIO.FALLING, callback=testCallback,bouncetime=300)
+GPIO.add_event_detect(22, GPIO.FALLING, callback=testButton,bouncetime=300)
 
+while True:
+	print "waiting..."
